238e3338a2718f7cc7655414917ee0fc1cfaa7c5
Damir Kotorić <damirkotoric@gmail.com>
2025-08-23 11:25:32 +0800

    wip

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,6 +1,6 @@
 // lib/strapi.ts
 export const STRAPI_URL =
-  process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337';
+  (process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337').replace(/\/$/, '');
 
 export async function strapiFetch<T = any>(path: string) {
   const res = await fetch(`${STRAPI_URL}${path}`, {
